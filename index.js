a234c4a9091669e5afbd6ea4ae7e302709b80515
Kyle Goddard <kyle@taskflow.co.za>
2022-03-03 14:05:59 +0200

    part 3: 3.7 - 3.8 done

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+morgan.token("data", function (req, res) {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  } else {
+    return null;
+  }
+});
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.data(req, res),
+    ].join(" ");
+  })
+);
 
 let phonebook = [
   {
