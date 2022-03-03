0dacb2463f8be6e329b1a92f3c50c3e44b52b84d
Kyle Goddard <kyle@taskflow.co.za>
2022-03-03 13:43:49 +0200

    part 3: 3.1-3.6 done

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let phonebook = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Phonebook</h1>");
+});
+
+app.get("/info", (request, response) => {
+  const info_str = `<div>Phonebook has info for ${
+    phonebook.length
+  } people.</div>
+  <div>${Date()}</div>`;
+
+  response.send(info_str);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebook);
+});
+
+app.post("/api/persons", (request, response) => {
+  const getNewID = () => {
+    let id = Math.random() * 10000000;
+
+    // const ids = phonebook.map((p) => p.id);
+
+    // while (ids.includes(id.toFixed(0))) {
+    //   id = Math.random() * 10000000;
+    // }
+
+    return Number(id.toFixed(0));
+  };
+  const person = request.body;
+
+  if (person.name && person.number) {
+    let existingNames = phonebook.map((p) => p.name.toUpperCase());
+
+    if (existingNames.includes(person.name.toUpperCase())) {
+      return response.status(400).json({
+        error: "name must be unique",
+      });
+    }
+    person.id = getNewID();
+
+    phonebook = phonebook.concat(person);
+
+    response.json(person);
+  } else {
+    return response.status(400).json({
+      error: "Missing name or number",
+    });
+  }
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const person = phonebook.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  phonebook = phonebook.filter((p) => p.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
